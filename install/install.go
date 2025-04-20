9922f486222105d98cb88af733367fc6d6df7a9b
Daniel <dhcgn@users.noreply.github.com>
2025-04-20 20:57:17 +0200

    Implement platform-specific context menu integration for JPEGLI Optimizer on Windows and add a no-op function for Linux

--- install/install.go
+++ install/install.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 
 	"github.com/dhcgn/jpegli-windows-explorer-extention/types"
-	"golang.org/x/sys/windows/registry"
 )
 
 // Embedded zip files
@@ -309,68 +308,6 @@ func CopyExecutableToProgramData() string {
 
 	fmt.Printf("Executable successfully copied to %s\n", targetPath)
 	return targetPath
-}
-
-// SetExecutableAsWindowsExplorerContextMenu sets the executable as a Windows Explorer context menu item
-// for Files and Folders.
-func SetExecutableAsWindowsExplorerContextMenu(execPath string) {
-	// Keep the path as is (don't convert to slashes) and properly escape it for the registry
-	execCommand := "\"" + execPath + "\" \"%1\""
-	// Registry keys to modify
-	registryKeys := []struct {
-		parent string
-		path   string
-		name   string
-		value  string
-	}{
-		// For all files
-		{`SOFTWARE\Classes\*\shell`, "JPEGLIOptimizer", "", "Optimize with JPEGLI"},
-		{`SOFTWARE\Classes\*\shell\JPEGLIOptimizer`, "Icon", "", execPath},
-		{`SOFTWARE\Classes\*\shell\JPEGLIOptimizer\command`, "", "", execCommand},
-
-		// For folders
-		{`SOFTWARE\Classes\Directory\shell`, "JPEGLIOptimizer", "", "Optimize JPEGs with JPEGLI"},
-		{`SOFTWARE\Classes\Directory\shell\JPEGLIOptimizer`, "Icon", "", execPath},
-		{`SOFTWARE\Classes\Directory\shell\JPEGLIOptimizer\command`, "", "", execCommand},
-	}
-
-	// Create or update registry entries
-	for _, key := range registryKeys {
-		// Try to open existing key first
-		fullPath := key.parent + "\\" + key.path
-		k, exists, err := registry.CreateKey(registry.CURRENT_USER, fullPath, registry.ALL_ACCESS)
-		if err != nil {
-			fmt.Printf("Error accessing registry key %s: %v\n", fullPath, err)
-			continue
-		}
-
-		// Set or update the registry value
-		if key.name == "" {
-			// Default value
-			err = k.SetStringValue("", key.value)
-			if err != nil {
-				fmt.Printf("Error setting default value for key %s: %v\n", fullPath, err)
-			} else if exists {
-				fmt.Printf("Updated existing registry key: %s\n", fullPath)
-			} else {
-				fmt.Printf("Created new registry key: %s\n", fullPath)
-			}
-		} else {
-			// Named value
-			err = k.SetStringValue(key.name, key.value)
-			if err != nil {
-				fmt.Printf("Error setting value '%s' for key %s: %v\n", key.name, fullPath, err)
-			} else if exists {
-				fmt.Printf("Updated existing registry value: %s\\%s\n", fullPath, key.name)
-			} else {
-				fmt.Printf("Created new registry value: %s\\%s\n", fullPath, key.name)
-			}
-		}
-
-		k.Close()
-	}
-
-	fmt.Println("Successfully updated JPEGLI Optimizer in Windows Explorer context menu")
 }
 
 // findExifTool searches for the exiftool executable in the given directory
